2774c4f8de7ec70a23a4e7e4da5a02627fa79329
Michael Samuel <mik@miknet.net>
2013-01-14 16:58:15 +1100

    Allow setting per-exec number of rounds

--- clotpmd5.py
+++ clotpmd5.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 
 # Simple python code to launch OpenCL otp-md5 chain calculator
- 
+
 import pyopencl as cl
 import numpy
 
-DEFAULT_ROUNDS=268435456 # 2 ** 28
-
-def calc_range(start, num, rounds):
+def calc_range(start, num, rounds, per_exec_rounds=None):
 	"""Calculate the otp-md5 of the 64-bit numbers range(start, num),
 	   with otp sequence of rounds."""
+
+	if per_exec_rounds is None:
+		per_exec_rounds = rounds
+
+	if rounds % per_exec_rounds != 0:
+		raise ValueError("rounds must be a multiple of perexec")
 
 	# Boilerplate OpenCL stuff
 	ctx = cl.create_some_context()
@@ -25,7 +29,8 @@ def calc_range(start, num, rounds):
 	dev_data = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=host_input)
 	
 	# Execute the kernel, and wait for it to finish
-	prg.otpmd5(queue, host_input.shape, None, dev_data, numpy.uint32(rounds)).wait()
+	for i in xrange(rounds / per_exec_rounds):
+		prg.otpmd5(queue, host_input.shape, None, dev_data, numpy.uint32(per_exec_rounds)).wait()
 
 	# Copy the results from the device
 	result = numpy.empty_like(host_input)
@@ -40,7 +45,9 @@ if __name__ == "__main__":
 	parser.add_argument("--start", help="First input OTP value", type=long, default=0L)
 	parser.add_argument("--num", help="Number of OTP chains to calculate", type=long, default=1024L)
 	parser.add_argument("--rounds", help="Number of rounds in each chain", type=long, default=2**28L)
+	parser.add_argument("--perexec", help="Number of rounds per-execution", type=long)
+
 	args = parser.parse_args()
-	for (source, result) in calc_range(args.start, args.num, args.rounds):
+	for (source, result) in calc_range(args.start, args.num, args.rounds, args.perexec):
 		print "%016x %016x" % (source, result)
 
