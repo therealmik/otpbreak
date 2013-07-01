23ae2b40db56205da428133cf23c910e8560708d
Michael Samuel <mik@miknet.net>
2013-07-01 15:00:32 +1000

    Don't require cython for setup.py

--- pyotpmd5/setup.py
+++ pyotpmd5/setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+#from Cython.Distutils import build_ext
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("cotpmd5", ["cotpmd5.pyx", "otpmd5.c"])],
-    #ext_modules = [Extension("cotpmd5", ["cotpmd5.c", "otpmd5.c"])],
+    #cmdclass = {'build_ext': build_ext},
+    #ext_modules = [Extension("cotpmd5", ["cotpmd5.pyx", "otpmd5.c"])],
+    ext_modules = [Extension("cotpmd5", ["cotpmd5.c", "otpmd5.c"])],
     py_modules = ["otpmd5"],
 )
